e8fa105d145ff194e0ff9945dcce1be6596c87c5
Ben Coleman <benc.uk@gmail.com>
2024-10-29 15:12:50 +0000

    Phi still not woking with transformers v3

--- phi-chat-transjs/phi-model.js
+++ phi-chat-transjs/phi-model.js
@@ -1,15 +1,18 @@
+import { checkCache } from '../lib/utils.js'
 import { addErrorMsg, addStatusMsg, showQueryControls } from './ui.js'
 
 // ***************************************************************
 // DOES NOT WORK YET!
 // transformers.js doesn't support the model & external data yet
+// https://github.com/xenova/transformers.js/issues/963
 // ***************************************************************
 
 // Using v3 of the Transformers library - still in alpha/beta
-import { env, pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.0-alpha.19'
+import { env, pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.0.1'
 
 const MODEL = 'microsoft/Phi-3-mini-4k-instruct-onnx-web'
 //const MODEL = 'Xenova/Phi-3-mini-4k-instruct'
+//const MODEL = 'schmuell/Phi-3-mini-4k-instruct-onnx-web'
 const USE_LOCAL_MODEL = false
 
 env.localModelPath = '../models'
@@ -19,10 +22,12 @@ env.allowLocalModels = USE_LOCAL_MODEL
 let phiPipeline = null
 
 export async function setUp() {
+  addStatusMsg('🧠 Loading model...')
+  checkCache('../models/phi-3-mini-4k-instruct.onnx')
   try {
     phiPipeline = await pipeline('text-generation', MODEL, {
       device: 'webgpu',
-      use_external_data_format: true,
+      // use_external_data_format: true,
       dtype: 'q4f16',
     })
 
